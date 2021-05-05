7869ab9b5d5e6bcce60ba3cc7595bc26afb792c1
Senya-hub <78022528+Senya-hub@users.noreply.github.com>
2021-05-05 23:27:56 +0900

    add module7.2

--- python/courses/module7.py
+++ python/courses/module7.py
@@ -67,3 +67,46 @@ elif (rating4 > rating5) and (rating4 > rating3):
   print('Хорошистов больше всех: ', rating4)
 else:
   print('Отличников больше всех: ', rating5)
+
+  a = int(input('Введите число а: '))
+b = int(input('Введите число b: '))
+i = 0
+total = 0
+for number in range(a, b):
+  if number % 3 == 0:
+    total += number
+    print('Накопительное кратное три - ', total)
+    i += 1
+amount = total // i
+print('Среднее арифметическое чисел кратное трем: ', amount)
+
+for number in range(10, 99):
+  if number % 3 == 0:
+    print('Двухзначные числа равные утроеному произведению: ', number)
+
+fun = 0
+i = 0
+for compare in range(1, 11):
+  number = int(input('Введите число: '))
+  if fun < number:
+    print('число упорядочено по возрастаию ', number)
+    fun = number
+    i += 1
+  else:
+    print('Число не упорядочено ', number)
+print()
+print('Упорядоченных месяцев: ', i) 
+
+card = int(input('Введите кол-во карточек: '))
+total_card = 0
+total_summ = 0
+for number in range(1, card + 1):
+  total_card += number
+print('Итоговая сумма: ', total_card)
+for summ_namber in range(1, card):
+  namber_card = int(input('Введите номера карточек: '))
+  total_summ += namber_card
+print('Сумма номеров карточек: ', total_summ)
+total_ask = total_card - total_summ
+print()
+print('Номер отсутсвующей карточки - ', total_ask)
