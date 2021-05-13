d028fe669511ae778bfef673a684d0fecccb1a93
Senya-hub <78022528+Senya-hub@users.noreply.github.com>
2021-05-13 10:00:56 +0900

    add module9

--- python/courses/module9.py
+++ python/courses/module9.py
@@ -43,4 +43,107 @@ for number in range(rows):
   print('=' * seats)
 
 
+print('Задача 5. Марсоход 2')
+print('В какую сторону направить робота - W(север), S(юг), A(запад), D(восток)')
+print('Выход из игры, любая другая клавиша!\n')
+x = 8
+y = 10
+get = True
 
+while get:
+  print('Марсоход находится на позции ', x, y, end = ' ')
+  traffic = input('введите команду:\n')
+  if traffic == 'w':
+    x += 1
+    if x >= 15:
+      x = 15
+  elif traffic == 's':
+    x -= 1
+    if x <= 0:
+      x = 0
+  elif traffic == 'd':
+    y += 1
+    if y >= 20:
+      y = 20
+  elif traffic == 'a':
+    y -= 1
+    if y <= 0:
+      y = 0
+  else:
+    print('Выход из игры!')
+    get = False
+
+
+print('Задача 6. Спецшифр')
+string = input('Введите строку: ')
+prevSyn = ''
+i = 1
+n = 1
+
+for letter in string:
+  if letter == prevSyn:
+    i += 1
+  elif letter != prevSyn:
+    prevSyn = letter
+    i = n
+  if i > n:
+    c = i
+print('Самая длинная последовательность: ', c)
+
+
+print('Задача 7. Великий и могучий')
+text = input('Введите текст: ')
+i = 1
+c = 0
+
+for letter in text:
+  if letter != ' ':
+    i += 1
+  elif letter == ' ':
+    i = 0
+  if i > c:
+    c = i
+ 
+print('Длина самого длинного слова: ', c)
+
+
+print('Задача 8. Колонтитул')
+footer = int(input('Введите общую длину колонтитула в символах: '))
+point = int(input('Введите кол-во восклицательных знаков: '))
+print('\n')
+
+for letter in range(footer):
+  if letter < (footer - point) // 2:
+    print('~', end = '')
+  elif letter < (footer + point) // 2:
+    print('!', end = '')
+  else:
+    print('~', end = '')
+print('\n')
+
+
+print('Задача 9. Коровы')
+string = input('Введите строку с привязкой коров к стоилу: ')
+i = 0
+m = 0
+
+for letter in string:
+  i += 2
+  if letter == 'b':
+    m += i
+print('Произведено молока за день: ', m)
+
+print('Задача 10. Метод бутерброда')
+letter = input('Введите зашифрованное сообщение: ')
+i = 0
+symbols = ''
+symbolw = ''
+
+for symbol in letter:
+  i += 1
+  if i % 2 != 0:
+    symbols += symbol
+  else:
+    symbolw = symbol + symbolw 
+
+print(symbols + symbolw) 
