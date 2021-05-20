fae2d9ac6a8a938fb37f84e68d188d0f16298ceb
Senya-hub <78022528+Senya-hub@users.noreply.github.com>
2021-05-20 15:01:50 +0900

    add files

--- python/courses/Homework/block13.py
+++ python/courses/Homework/block13.py
@@ -244,3 +244,25 @@ thirdNumber = float(input('Введите третье число: '))
 count = egv(firstNumber, secondNumber, thirdNumber)
 
 print(count)
+
+
+# Число Эйлера
+import math
+print(math.e)
+
+# Число Эйлера 2
+import math
+
+precision = float(input('Введите точность: '))
+
+result = 0
+i = 0
+addMember = 1
+
+while addMember > precision:
+  addMember = 1 / math.factorial(i)
+  result += addMember
+  i += 1
+
+print('Результат: ', result)
+print('Котстанта: ', math.e)
